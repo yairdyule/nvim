c0205a092ee8a0411ad5a88092b88753b34cef5b
Jared Jewell <jaje9434@colorado.edu>
2021-06-17 20:16:59 -0600

    treesitter, settings, mappings

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 require "mappings"
 require "plugins"
 require "telescope_config"
+require "treesitter_config"
 
 local cmd = vim.cmd
 local g = vim.g
 
 g.mapleader = ","
+g.maplocalleader = ","
 
 cmd "syntax on"
 cmd "colo gruvbox-flat"
